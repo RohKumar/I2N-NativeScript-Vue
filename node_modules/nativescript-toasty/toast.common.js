045bce0dd03d5364d33ae36f4547ec4d11c36140
Aman <amad0018@student.monash.edu>
2020-06-27 19:17:14 +1000

    commit

---
+++ node_modules/nativescript-toasty/toast.common.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ToastDuration;
+(function (ToastDuration) {
+    ToastDuration["SHORT"] = "short";
+    ToastDuration["LONG"] = "long";
+})(ToastDuration = exports.ToastDuration || (exports.ToastDuration = {}));
+var ToastPosition;
+(function (ToastPosition) {
+    ToastPosition["BOTTOM"] = "bottom";
+    ToastPosition["BOTTOM_LEFT"] = "bottomLeft";
+    ToastPosition["BOTTOM_RIGHT"] = "bottomRight";
+    ToastPosition["CENTER"] = "center";
+    ToastPosition["CENTER_LEFT"] = "centerLeft";
+    ToastPosition["CENTER_RIGHT"] = "centerRight";
+    ToastPosition["TOP"] = "top";
+    ToastPosition["TOP_LEFT"] = "topLeft";
+    ToastPosition["TOP_RIGHT"] = "topRight";
+})(ToastPosition = exports.ToastPosition || (exports.ToastPosition = {}));
+//# sourceMappingURL=toast.common.js.map
