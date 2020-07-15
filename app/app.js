5e3fabc501d84841662043c437a6aff6fe85b523
Aman <amad0018@student.monash.edu>
2020-07-15 10:25:34 +1000

    commit

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import Vue from 'nativescript-vue';
 import Home from './components/Home';
 
 // Uncommment the following to see NativeScript-Vue output logs
-// Vue.config.silent = false;
+ //Vue.config.silent = true;
 
 
 
