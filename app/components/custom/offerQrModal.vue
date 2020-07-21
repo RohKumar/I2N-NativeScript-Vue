082586f0ebd09076c2321f8f521ed5a7e9011855
RISHABH\Fathullah.Mohammedi <fathullah.mohammedi@rishabhsoft.com>
2020-07-21 13:35:27 +0530

    Added offers qr modal

---
+++ app/components/custom/offerQrModal.vue
@@ -0,0 +1,17 @@
+<template>
+	<StackLayout class="p-15" backgroundColor="white">
+		<Image src="~/assets/images/qr.png" stretch="none" />
+		<Button class="btn btn-outline" text="Close Modal" @tap="$modal.close()" />
+	</StackLayout>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style>
+</style>
